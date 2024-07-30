d26727106d4cffd26cb5e558f73f17b099d74018
Kisahn <117547616+Kisahn@users.noreply.github.com>
2024-07-30 18:58:22 +0200

    Fix, Input Polling, Key & Mouse Codes

--- Calavera/src/Calavera.h
+++ Calavera/src/Calavera.h
@@ -1,16 +1,21 @@
 #ifndef CALAVERA_H
 #define CALAVERA_H
 
-	// For use by Calavera applications
+// For use by Calavera applications
 
-	#include "Calavera/Application.h"
-	#include "Calavera/Layer.h"
-	#include "Calavera/Log.h"
-	#include "Calavera/ImGui/ImGuiLayer.h"
+#include "Calavera/Application.h"
+#include "Calavera/Layer.h"
+#include "Calavera/Log.h"
 
-	// --- Entry Point ---------------------
-	#include "Calavera/EntryPoint.h"
-	// -------------------------------------
+#include "Calavera/Input.h"
+#include "Calavera/KeyCodes.h"
+#include "Calavera/MouseButtonCodes.h"
+
+#include "Calavera/ImGui/ImGuiLayer.h"
+
+// --- Entry Point ---------------------
+#include "Calavera/EntryPoint.h"
+// -------------------------------------
 
 
 #endif // !CALAVERA_H
