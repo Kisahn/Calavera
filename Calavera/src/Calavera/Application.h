5c5fd615430f20dd1ab196d0d2568863004268a8
Kisahn <117547616+Kisahn@users.noreply.github.com>
2024-08-03 14:38:03 +0200

    OpenGL Shader

--- Calavera/src/Calavera/Application.h
+++ Calavera/src/Calavera/Application.h
@@ -10,6 +10,7 @@
 
 #include "Calavera/ImGui/ImGuiLayer.h"
 
+#include "Calavera/Renderer/Shader.h"
 
 namespace Calavera {
 
@@ -39,6 +40,7 @@ namespace Calavera {
 			LayerStack m_LayerStack;
 
 			unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+			std::unique_ptr<Shader> m_Shader;
 
 			static Application* s_Instance;
 	};
