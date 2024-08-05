39bfc819b65387dd56f701263327c7217c9659a9
Kisahn <117547616+Kisahn@users.noreply.github.com>
2024-08-05 10:10:45 +0200

    Transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Calavera::Layer
 {
 	public:
@@ -33,10 +35,10 @@ class ExampleLayer : public Calavera::Layer
 			m_SquareVA.reset(Calavera::VertexArray::Create());
 
 			float squareVertices[3 * 4] = {
-				-0.75f, -0.75f, 0.0f,
-				 0.75f, -0.75f, 0.0f,
-				 0.75f,  0.75f, 0.0f,
-				-0.75f,  0.75f, 0.0f
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.5f,  0.5f, 0.0f,
+				-0.5f,  0.5f, 0.0f
 			};
 
 			std::shared_ptr<Calavera::VertexBuffer> squareVB;
@@ -58,6 +60,7 @@ class ExampleLayer : public Calavera::Layer
 				layout(location = 1) in vec4 a_Color;
 
 				uniform mat4 u_ViewProjection;
+				uniform mat4 u_Transform;
 
 				out vec3 v_Position;
 				out vec4 v_Color;
@@ -66,7 +69,7 @@ class ExampleLayer : public Calavera::Layer
 				{
 					v_Position = a_Position;
 					v_Color = a_Color;
-					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 				}			
 			)";
 
@@ -93,13 +96,14 @@ class ExampleLayer : public Calavera::Layer
 				layout(location = 0) in vec3 a_Position;
 
 				uniform mat4 u_ViewProjection;
+				uniform mat4 u_Transform;
 
 				out vec3 v_Position;
 
 				void main()
 				{
 					v_Position = a_Position;
-					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 				}			
 			)";
 
@@ -121,8 +125,6 @@ class ExampleLayer : public Calavera::Layer
 
 		void OnUpdate(Calavera::Timestep ts) override
 		{
-			CV_TRACE("Delta time: {0}s ({1} ms)", ts.GetSeconds(), ts.GetMilliseconds());
-
 			if (Calavera::Input::IsKeyPressed(CV_KEY_LEFT))
 				m_CameraPosition.x -= m_CameraMoveSpeed * ts;
 			else if (Calavera::Input::IsKeyPressed(CV_KEY_RIGHT))
@@ -146,7 +148,18 @@ class ExampleLayer : public Calavera::Layer
 
 			Calavera::Renderer::BeginScene(m_Camera);
 
-			Calavera::Renderer::Submit(m_BlueShader, m_SquareVA);
+			static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+			for (int y = 0; y < 20; y++)
+			{
+				for (int x = 0; x < 20; x++)
+				{
+					glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+					glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+					Calavera::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				}
+			}
+
 			Calavera::Renderer::Submit(m_Shader, m_VertexArray);
 
 			Calavera::Renderer::EndScene();
