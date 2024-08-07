eb5782db3406e74978f185db6eacd09b4d259cc8
Kisahn <117547616+Kisahn@users.noreply.github.com>
2024-08-07 10:46:51 +0200

    Shader asset files

--- Calavera/src/Calavera/Renderer/Shader.cpp
+++ Calavera/src/Calavera/Renderer/Shader.cpp
@@ -8,6 +8,18 @@
 
 namespace Calavera {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:       CV_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::OpenGL:     return new OpenGLShader(filepath);
+		}
+
+		CV_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -128,40 +128,7 @@ class ExampleLayer : public Calavera::Layer
 
 			m_FlatColorShader.reset(Calavera::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
-			std::string textureShaderVertexSrc = R"(
-				#version 330 core
-
-				layout(location = 0) in vec3 a_Position;
-				layout(location = 1) in vec2 a_TextCoord;
-
-				uniform mat4 u_ViewProjection;
-				uniform mat4 u_Transform;
-
-				out vec2 v_TextCoord;
-
-				void main()
-				{
-					v_TextCoord = a_TextCoord;
-					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-				}			
-			)";
-
-			std::string textureShaderFragmentSrc = R"(
-				#version 330 core
-
-				layout(location = 0) out vec4 color;
-
-				in vec2 v_TextCoord;
-
-				uniform sampler2D u_Texture;
-
-				void main()
-				{
-					color = texture(u_Texture, v_TextCoord);
-				}			
-			)";
-
-			m_TextureShader.reset(Calavera::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+			m_TextureShader.reset(Calavera::Shader::Create("assets/shaders/Texture.glsl"));
 
 			m_Texture = Calavera::Texture2D::Create("assets/textures/Checkerboard.png");
 			m_CalaveraLogoTexture = Calavera::Texture2D::Create("assets/textures/CalaveraLogo.png");
