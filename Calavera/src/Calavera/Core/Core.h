7a45316ba1dca3e954399215834325165c4cb015
Kisahn <117547616+Kisahn@users.noreply.github.com>
2024-08-15 15:02:51 +0200

    Single Shader 2D Renderer & Core update

--- Calavera/src/Calavera/Core/Core.h
+++ Calavera/src/Calavera/Core/Core.h
@@ -3,6 +3,48 @@
 
 #include <memory>
 
+// Platform detection using predefined macros
+#ifdef _WIN32
+	/* Windows x64/x86 */
+	#ifdef _WIN64
+		/* Windows x64 */
+		#define CV_PLATFORM_WINDOWS
+	#else
+		/* Windows x86 */
+		#error "x86 Builds are not supported!"
+	#endif
+	#elif defined (__APPLE__) || defined(__MACH__)
+		#include <TargetConditionals.h>
+		/*  TARGET_OS_MAC exists on all the platforms
+		 *  so we must check all of them (in this order)
+		 *  to ensure that we're running on MAX
+		 *  and not some other Apple platform */
+		#if TARGET_IPHONE_SIMULATOR == 1
+			#error "IOS simulator is not supported!"
+		#elif TARGET_OS_IPHONE == 1
+			#define CV_PLATFORM_IOS
+			#error "IOS is not supported!"
+		#elif TARGET_OS_MAC == 1
+			#define CV_PLATFORM_MACOS
+			#define "MacOS is not supported!"
+		#else
+			#error "Unknown Apple platform!"
+		#endif
+		/* We also have to check __ANDROID__ before __linux__
+		* since android is based on the linux kernel
+		* it has __linux__ defined */
+		#elif defined (__ANDROID__)
+			#define CV_PLATFORM_ANDROID
+			#error "Android is not supported!"
+		#elif defined (__linux__)
+			#define CV_PLATFORM_LINUX
+			#error "Linux is not supported!"
+		#else
+			/* Unknow compiler/platform */
+			#error "Unknown platform!"
+#endif // End of platform detection
+
+// DLL support
 #ifdef CV_PLATFORM_WINDOWS
 	#if CV_DYNAMIC_LINK
 		#ifdef CV_BUILD_DLL
@@ -15,11 +57,11 @@
 	#endif
 #else
 	#error Calavera only supports Windows !
-#endif
+#endif // End of DLL support
 
 #ifdef CV_DEBUG
 	#define CV_ENABLE_ASSERTS
-#endif // CV_DEBUG
+#endif
 
 
 #ifdef CV_ENABLE_ASSERTS
@@ -28,7 +70,7 @@
 #else
 	#define CV_ASSERT(x, ...)
 	#define CV_CORE_ASSERT(x, ...)
-#endif // CV_ENABLE_ASSERTS
+#endif
 
 #define BIT(x) (1 << x)
 
@@ -38,9 +80,19 @@ namespace Calavera {
 
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
 
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
 
 }
 
