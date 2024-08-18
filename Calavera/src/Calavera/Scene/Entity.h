aaf2eaf3394e6439bfe23ddcd41432a018dfa544
Kisahn <117547616+Kisahn@users.noreply.github.com>
2024-08-19 00:29:30 +0200

    Add/Remove Entities and components UI

--- Calavera/src/Calavera/Scene/Entity.h
+++ Calavera/src/Calavera/Scene/Entity.h
@@ -19,7 +19,9 @@ namespace Calavera {
 			T& AddComponent(Args&&... args)
 			{
 				CV_CORE_ASSERT(!HasComponent<T>(), "Entity already has component!");
-				return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+				T& component = m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+				m_Scene->OnComponentAdded<T>(*this, component);
+				return component;
 			}
 
 			template<typename T>
@@ -43,6 +45,7 @@ namespace Calavera {
 			}
 
 			operator bool() const { return m_EntityHandle != entt::null; }
+			operator entt::entity() const { return m_EntityHandle; }
 			operator uint32_t() const { return (uint32_t)m_EntityHandle; }
 
 			bool operator==(const Entity& other) const
