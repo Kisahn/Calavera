6f9309e99e9f0a4d36375e5c1c3e15e138bc5a68
Kisahn <jdeck.dw@gmail.com>
2025-04-03 23:26:58 +0200

    Premake
    Add premake to the engine. Creation of a .bat file used to generate all
    of the projects parameters easily

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Calavera"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Calavera"
+	location "Calavera"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+
+		defines
+		{
+			"CV_PLATFORM_WINDOWS",
+			"CV_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CV_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "CV_DIST"
+		symbols "On"
+
+	filter "action:vs*"
+        buildoptions { "/utf-8" }
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Calavera/vendor/spdlog/include",
+		"Calavera/src"
+	}
+
+	links
+	{
+		"Calavera"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+
+		defines
+		{
+			"CV_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CV_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CV_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "CV_DIST"
+		symbols "On"
+
+	filter "action:vs*"
+        buildoptions { "/utf-8" }
