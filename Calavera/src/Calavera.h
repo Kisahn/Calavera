0280514364b94d39052efa9f1a5d62080480bdd8
Kisahn <jdeck.dw@gmail.com>
2025-04-07 22:04:17 +0200

    Key and Mouse codes

--- Calavera/src/Calavera.h
+++ Calavera/src/Calavera.h
@@ -6,6 +6,10 @@
 #include "Calavera/Layer.h"
 #include "Calavera/Log.h"
 
+#include "Calavera/Input.h"
+#include "Calavera/KeyCodes.h"
+#include "Calavera/MouseButtonCodes.h"
+
 #include "Calavera/ImGui/ImGuiLayer.h"
 
 //------------- EntryPoint ----------------
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,12 +10,19 @@ class ExampleLayer : public Calavera::Layer
 
 		void OnUpdate() override
 		{
-			CV_INFO("ExampleLayer::Update");
+			if (Calavera::Input::IsKeyPressed(CV_KEY_TAB))
+				CV_TRACE("Tab key is pressed (poll) !");
 		}
 
 		void OnEvent(Calavera::Event& event) override
 		{
-			CV_TRACE("{0}", event);
+			if (event.GetEventType() == Calavera::EventType::KeyPressed)
+			{
+				Calavera::KeyPressedEvent& e = (Calavera::KeyPressedEvent&)event;
+				if (e.GetKeyCode() == CV_KEY_TAB)
+					CV_TRACE("Tab key is pressed (event) !");
+				CV_TRACE("{0}", (char)e.GetKeyCode());
+			}
 		}
 
 };
