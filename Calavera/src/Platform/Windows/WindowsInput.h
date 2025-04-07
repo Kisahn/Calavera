2c4d1eddb5cd80fd41f12c6a2388d329de540718
Kisahn <jdeck.dw@gmail.com>
2025-04-07 21:52:37 +0200

    Input Polling

---
+++ Calavera/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Calavera/Input.h"
+
+namespace Calavera {
+
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+
+}
