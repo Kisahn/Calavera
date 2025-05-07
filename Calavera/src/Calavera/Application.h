de81fa148c4b006c18efc0277ec4969498f48405
Kisahn <jdeck.dw@gmail.com>
2025-05-07 15:59:54 +0200

    The first triangle

--- Calavera/src/Calavera/Application.h
+++ Calavera/src/Calavera/Application.h
@@ -13,29 +13,33 @@ namespace Calavera {
 
 	class CALAVERA_API Application
 	{
-	public:
-		Application();
-		virtual ~Application();
+		public:
+			Application();
+			virtual ~Application();
 
-		void Run();
+			void Run();
 
-		void OnEvent(Event& e);
+			void OnEvent(Event& e);
 
-		void PushLayer(Layer* layer);
-		void PushOverlay(Layer* layer);
+			void PushLayer(Layer* layer);
+			void PushOverlay(Layer* layer);
 
-		inline Window& GetWindow() { return *m_Window; }
+			inline Window& GetWindow() { return *m_Window; }
 
-		inline static Application& Get() { return *s_Instance; }
-	private:
-		bool OnWindowClose(WindowCloseEvent& e);
+			inline static Application& Get() { return *s_Instance; }
 
-		std::unique_ptr<Window> m_Window;
-		ImGuiLayer* m_ImGuiLayer;
-		bool m_Running = true;
-		LayerStack m_LayerStack;
-	private:
-		static Application* s_Instance;
+		private:
+			bool OnWindowClose(WindowCloseEvent& e);
+
+			std::unique_ptr<Window> m_Window;
+			ImGuiLayer* m_ImGuiLayer;
+			bool m_Running = true;
+			LayerStack m_LayerStack;
+
+			unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+
+		private:
+			static Application* s_Instance;
 	};
 
 	// To be defined in CLIENT
--- Calavera/src/Platform/OpenGL/OpenGLContext.cpp
+++ Calavera/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace Calavera{
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		CV_CORE_ASSERT(status, "Failed to initialize Glad !");
+
+		CV_CORE_INFO("OpenGL Info:");
+		CV_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		CV_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		CV_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
